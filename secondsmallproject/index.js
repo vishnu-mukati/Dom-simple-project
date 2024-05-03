ae5d53f195d38b687875c6381fb9279a3ef878dd
vishnu mukati <mukativishnu1232@gmail.com>
2024-05-03 15:02:48 +0530

    query selector and query selector all methods

--- secondsmallproject/index.js
+++ secondsmallproject/index.js
@@ -5,3 +5,43 @@ for(let i=0;i<listname.length;i++)
   {
     listname[i].style.fontStyle = 'italic';
   }
+
+  let mainheding = document.querySelector('#main-heading');
+mainheding.style.textAlign = 'center';
+
+let fruits = document.querySelector('.fruit');
+fruits.style.backgroundColor = 'grey';
+fruits.style.padding = '30px';
+fruits.style.margin = '30px';
+fruits.style.width ='50%';
+fruits.style.borderRadius = '5px';
+fruits.style.listStyleType ='none';
+
+let basketheading = document.querySelector('h2');
+basketheading.style.marginleft ='30px';
+
+let fruititems = document.querySelectorAll('.fruit');
+for(let i=0;i<fruititems.length;i++)
+  {
+    fruititems[i].style.backgroundColor ='white';
+    fruititems[i].style.padding ='10px';
+    fruititems[i].style.margin  = '10px';
+    fruititems[i].style.borderRadius = '5px';
+  }
+
+let oddfruititems = document.querySelectorAll('.fruit:nth-child(odd)');
+for(let i=0;i<oddfruititems.length;i++)
+  {
+    oddfruititems[i].style.backgroundColor = 'lightgray';
+  }
+// Write answer to the questions asked below:
+
+let basket = document.querySelector('#basket-heading');
+basket.style.color = 'brown';
+
+let fr = document.querySelectorAll('.fruit:nth-child(even)');
+for(let i=0;i<fr.length;i++)
+  {
+      fr[i].style.backgroundColor = 'brown';
+      fr[i].style.color = 'white';
+  }
