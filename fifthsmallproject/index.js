2e4f7dd818ad4ded53b3535eba4f0274ee642980
vishnu mukati <mukativishnu1232@gmail.com>
2024-05-11 20:16:56 +0530

    adding description and event listner to it

--- fifthsmallproject/index.js
+++ fifthsmallproject/index.js
@@ -13,3 +13,31 @@ input.appendChild(inputtext);
 let button = form.querySelector('button');
 form.insertBefore(input, button);
 
+//adding description
+let fruititems = document.querySelector('.fruits');
+let descriptionitems = document.querySelector('#description')
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    // Ensure that a description is provided
+    let fruitDescription = document.getElementById('description').value;
+    if (fruitDescription === '') {
+        alert('Please enter a description.');
+        return;
+    }
+     
+    let descriptionPara = document.getElementById('description');
+    // Adding the fruit name to the list
+    let fruitName = document.getElementById('fruit-to-add').value;
+    let newli = document.createElement('li');
+    newli.className = 'fruit';
+    newli.innerHTML = fruitName + "<p>" + descriptionPara.value + "</p>" + '<button class="delete-btn">x</button>'//3
+    newli.firstElementChild.style.fontStyle = "italic";
+    fruititems.appendChild(newli);
+    
+    // Clear input fields
+    document.getElementById('fruit-to-add').value = '';
+    document.getElementById('description').value = '';
+});
