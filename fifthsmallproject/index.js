949ed8f7ace1aae4bf84c0d6ef9661bdf18e9235
vishnu mukati <mukativishnu1232@gmail.com>
2024-05-13 20:46:32 +0530

    adding filter functionality

--- fifthsmallproject/index.js
+++ fifthsmallproject/index.js
@@ -5,8 +5,8 @@ let form = document.getElementsByTagName('form')[0];
 let input = document.createElement('input');
 let inputtext = document.createTextNode('description')
 input.id = 'description';
-input.setAttribute('type','text');
-input.setAttribute('placeholder' , 'description-to-add')
+input.setAttribute('type', 'text');
+input.setAttribute('placeholder', 'description-to-add')
 input.appendChild(inputtext);
 
 // inserbefor
@@ -20,14 +20,14 @@ let descriptionitems = document.querySelector('#description')
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    
+
     // Ensure that a description is provided
     let fruitDescription = document.getElementById('description').value;
     if (fruitDescription === '') {
         alert('Please enter a description.');
         return;
     }
-     
+
     let descriptionPara = document.getElementById('description');
     // Adding the fruit name to the list
     let fruitName = document.getElementById('fruit-to-add').value;
@@ -36,8 +36,35 @@ form.addEventListener('submit', function (event) {
     newli.innerHTML = fruitName + "<p>" + descriptionPara.value + "</p>" + '<button class="delete-btn">x</button>'//3
     newli.firstElementChild.style.fontStyle = "italic";
     fruititems.appendChild(newli);
-    
+
     // Clear input fields
     document.getElementById('fruit-to-add').value = '';
     document.getElementById('description').value = '';
 });
+// add delete functionality
+fruititems.addEventListener('click', function (event) {
+    if (event.target.classList.contains('delete-btn')) {
+        let buttontodelete = event.target.parentElement;
+        fruititems.removeChild(buttontodelete)
+    }
+})
+
+
+let filter = document.getElementById('filter');
+
+// grab the filter input and listen for the key up event
+
+filter.addEventListener('keyup', function(event) {
+    let textenterd = event.target.value.toLowerCase();
+    let fruititems = document.getElementsByClassName('fruit');
+    for(let i=0;i<fruititems.length;i++){
+        let currentfruittext = fruititems[i].firstChild.textContent.toLowerCase();
+        if(currentfruittext.indexOf(textenterd) == -1){
+            fruititems[i].style.display = 'none';
+        }else{
+            fruititems[i].style.display = 'flex';
+        }
+    }
+})
+
+
