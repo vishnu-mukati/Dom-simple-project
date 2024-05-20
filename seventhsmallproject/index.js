f4120a8d3b3717d64fc9351abfc33f1f748e67f6
vishnu mukati <mukativishnu1232@gmail.com>
2024-05-20 10:59:24 +0530

    convert the data as a string to a object by parse method

--- seventhsmallproject/index.js
+++ seventhsmallproject/index.js
@@ -8,11 +8,16 @@ function handleFormSubmit(event){
     let myobj = {
       username : username,
       email : email,
-      phone : phone
+      phone : phone,
     }
   
   let myobj_serialized = JSON.stringify(myobj);
+
   
   localStorage.setItem('User Details', myobj_serialized);
-    
+
+// JSON.parse is used to deserialized means convert the string to an object
+  let myobj_deserialized = JSON.parse(localStorage.getItem("User Details"));
+   
+    console.log(myobj_deserialized)
   }
