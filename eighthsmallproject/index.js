b5ca27c1c3dc69b8830b73887c8359d0a5a666e2
vishnu mukati <mukativishnu1232@gmail.com>
2024-05-21 21:44:43 +0530

    adding the showon screen feature of user details

--- eighthsmallproject/index.js
+++ eighthsmallproject/index.js
@@ -25,38 +25,10 @@ function showUserOnScreen(obj) {
   let parentEle = document.getElementById('userList');
   let childEle = document.createElement('li');
   childEle.textContent = obj.email + ' - ' + obj.username + ' - ' + obj.phone
-  // parentEle.innerHTML += `<li>${obj.username} - ${obj.email} - ${obj.phone}</li>`;
+  // parentEle.innerHTML += `<li>${obj.username} - ${obj.email} - ${obj.phone</li>`;
 
 
 
-  let deletebtn = document.createElement('button');
-  let deletebtntext = document.createTextNode('delete');
-  deletebtn.type = 'button';
-  deletebtn.value = 'Delete';
-  deletebtn.appendChild(deletebtntext);
-  deletebtn.onclick = () => {
-    localStorage.removeItem(obj.email);
-    parentEle.removeChild(childEle);
-  }
-  childEle.appendChild(deletebtn);
-  parentEle.appendChild(childEle);
-
-  let editbtn = document.createElement('button');
-  let editbtntext = document.createTextNode('edit');
-  editbtn.type = 'button';
-  editbtn.value = 'Edit';
-  editbtn.appendChild(editbtntext);
-  editbtn.onclick = () => {
-    edituserdetails(obj);
-  }
-  childEle.appendChild(editbtn);
-
-}
-
-function edituserdetails(obj){
-  let storedObj = JSON.parse(localStorage.getItem(obj.email));
-  localStorage.removeItem(obj.email);
   
-  console.log(storedObj)
 
 }
